19745c07b3ab44565ccfbd0ef7bfbebb7f9c9567
Eunni <49473766+eunSoft@users.noreply.github.com>
2023-11-30 12:42:57 +0900

    Title: 진법 변환, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 231130/진법 변환/base-conversion.java
+++ 231130/진법 변환/base-conversion.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        String n = sc.next();
         int m = sc.nextInt();
 
-        System.out.println(Integer.parseInt(Integer.toString(n),m));
+        System.out.println(Integer.parseInt(n,m));
     }
 }
