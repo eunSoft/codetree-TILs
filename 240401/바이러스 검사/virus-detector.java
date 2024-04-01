14478ebfb8cee4580bd243052438eb49e7aa30b2
Eunni <49473766+eunSoft@users.noreply.github.com>
2024-04-01 20:56:34 +0900

    Title: 바이러스 검사, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240401/바이러스 검사/virus-detector.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int MAX_N = 1000000;
+    public static int n;
+    public static int pm,pl;
+    public static List<Integer> map = new ArrayList<>();
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            map.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        pm = Integer.parseInt(st.nextToken());
+        pl = Integer.parseInt(st.nextToken());
+
+        System.out.println(getMin());
+
+
+    }
+
+    public static Long getMin(){
+        long result = 1;
+        for(int i=0; i<map.size(); i++){
+            int people = map.get(i)-pm;
+            if(people < 0 ) continue;
+            result += people / pl; 
+            if(people % pl !=0) result++;
+        }
+        
+        return result;
+    }
+}
